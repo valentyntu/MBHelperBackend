f6377d8a79ada832ce7071cb1b65a1108820a7e8
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-07-23 15:08:01 +0300

    Add: field sub to save model that represents user id.

--- models/TradingTableSave.js
+++ models/TradingTableSave.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const TradingTableSaveSchema = new mongoose.Schema({
     cities: {type: Array, required: true},
     products: {type: Array, required: true},
-    prices: {type: Array, required: true}
+    prices: {type: Array, required: true},
+    sub: {type: String}
 });
 
 const TradingTableSave = mongoose.model('TradingTableSave', TradingTableSaveSchema, 'saves');
